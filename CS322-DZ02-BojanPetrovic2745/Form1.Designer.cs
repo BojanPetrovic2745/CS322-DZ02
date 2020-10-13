77f21b6ce3b02b4a4573b4603b5dfcb769be089b
bojan <bojan.petrovic.2745@outlook.com>
2020-10-13 11:12:20 +0200

    DZ02

---
+++ CS322-DZ02-BojanPetrovic2745/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CS322_DZ02_BojanPetrovic2745
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.tbPrezime = new System.Windows.Forms.TextBox();
+            this.tbAdresa = new System.Windows.Forms.TextBox();
+            this.tbIme = new System.Windows.Forms.TextBox();
+            this.lblAdresa = new System.Windows.Forms.Label();
+            this.lblPrezime = new System.Windows.Forms.Label();
+            this.lblIme = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnesi.Location = new System.Drawing.Point(343, 214);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(75, 27);
+            this.btnUnesi.TabIndex = 13;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // tbPrezime
+            // 
+            this.tbPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPrezime.Location = new System.Drawing.Point(318, 139);
+            this.tbPrezime.Name = "tbPrezime";
+            this.tbPrezime.Size = new System.Drawing.Size(100, 26);
+            this.tbPrezime.TabIndex = 12;
+            // 
+            // tbAdresa
+            // 
+            this.tbAdresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAdresa.Location = new System.Drawing.Point(318, 171);
+            this.tbAdresa.Name = "tbAdresa";
+            this.tbAdresa.Size = new System.Drawing.Size(100, 26);
+            this.tbAdresa.TabIndex = 11;
+            // 
+            // tbIme
+            // 
+            this.tbIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbIme.Location = new System.Drawing.Point(318, 108);
+            this.tbIme.Name = "tbIme";
+            this.tbIme.Size = new System.Drawing.Size(100, 26);
+            this.tbIme.TabIndex = 10;
+            // 
+            // lblAdresa
+            // 
+            this.lblAdresa.AutoSize = true;
+            this.lblAdresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdresa.Location = new System.Drawing.Point(218, 177);
+            this.lblAdresa.Name = "lblAdresa";
+            this.lblAdresa.Size = new System.Drawing.Size(64, 20);
+            this.lblAdresa.TabIndex = 9;
+            this.lblAdresa.Text = "Adresa:";
+            // 
+            // lblPrezime
+            // 
+            this.lblPrezime.AutoSize = true;
+            this.lblPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrezime.Location = new System.Drawing.Point(218, 145);
+            this.lblPrezime.Name = "lblPrezime";
+            this.lblPrezime.Size = new System.Drawing.Size(70, 20);
+            this.lblPrezime.TabIndex = 8;
+            this.lblPrezime.Text = "Prezime:";
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIme.Location = new System.Drawing.Point(218, 114);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(40, 20);
+            this.lblIme.TabIndex = 7;
+            this.lblIme.Text = "Ime:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.tbPrezime);
+            this.Controls.Add(this.tbAdresa);
+            this.Controls.Add(this.tbIme);
+            this.Controls.Add(this.lblAdresa);
+            this.Controls.Add(this.lblPrezime);
+            this.Controls.Add(this.lblIme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.TextBox tbPrezime;
+        private System.Windows.Forms.TextBox tbAdresa;
+        private System.Windows.Forms.TextBox tbIme;
+        private System.Windows.Forms.Label lblAdresa;
+        private System.Windows.Forms.Label lblPrezime;
+        private System.Windows.Forms.Label lblIme;
+    }
+}
+
---
+++ CS322-DZ02-BojanPetrovic2745/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS322_DZ02_BojanPetrovic2745
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            
+            
+            string ime = tbIme.Text;
+            string prezime = tbPrezime.Text;
+            string adresa = tbAdresa.Text;
+
+            if(isImeAdresaValid(ime, adresa))
+            {
+                MessageBox.Show("Ime je validno, podaci su " +  ime + " " + prezime + " " + adresa);
+            }
+            else
+            {
+                MessageBox.Show("Ime i adresa nisu validni! Ime mora biti jedna reč, adresa više od jedne reči");
+            }
+        }
+
+        public static bool isImeAdresaValid(string ime, string adresa)
+        {
+            string regexIme = @"^\b[a-zA-Z0-9_]+\b$";
+            string regexAdresa = @"\w\s\w";
+
+            Regex reIme = new Regex(regexIme);
+            Regex reAdresa = new Regex(regexAdresa);
+
+            if (reIme.IsMatch(ime) && reAdresa.IsMatch(adresa))
+                return (true);
+            else
+                return (false);
+        }
+
+    }
+}
